4268e0479f5f3b235986cc1d2a42f0e9631e343e
matsalinas <matsalinas@uchile.cl>
2021-03-11 14:13:25 +0000

    Update tests koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,12 +2,12 @@
 pip install -e git+https://github.com/eol-uchile/uchileedxlogin@a11973e7625e1a3e66103ccad79c26103381988a#egg=uchileedxlogin
 pip install -e /openedx/requirements/edx-userdata
 
-cd /openedx/requirements/edx-userdata/edxuserdata
+cd /openedx/requirements/edx-userdata
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/edx-userdata/edxuserdata
+cd /openedx/requirements/edx-userdata
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest edxuserdata/tests.py
